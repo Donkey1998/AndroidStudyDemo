3d70407f6276094bddd0735b6ee02bbd041c054c
Tony.li <tony.li@gaiaworks.cn>
2020-03-20 17:06:10 +0800

    添加VolleyButton和监听事件

--- app/src/main/java/com/example/androidstudydemo/MainActivity.java
+++ app/src/main/java/com/example/androidstudydemo/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.androidstudydemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.volley_bt);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.volley_bt:
+                Toast.makeText(MainActivity.this,"Volley学习",Toast.LENGTH_LONG).show();
+                break;
+            default:break;
+        }
     }
 }

cf1eac2eadae2b7e51f10887f11f9b961d9e3e47
Tony.li <tony.li@gaiaworks.cn>
2020-03-23 16:35:59 +0800

    Base64与Video的互转:布局添加

--- app/src/main/java/com/example/androidstudydemo/MainActivity.java
+++ app/src/main/java/com/example/androidstudydemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.androidstudydemo.VideoToBase64Demo.VideoToBase64Demo;
 import com.example.androidstudydemo.VolleyDemo.VolleyActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -16,8 +17,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.volley_bt);
-        button.setOnClickListener(this);
+        Button volley_bt = findViewById(R.id.volley_bt);
+        volley_bt.setOnClickListener(this);
+        Button videoToBase64_bt = findViewById(R.id.videoToBase64_bt);
+        videoToBase64_bt.setOnClickListener(this);
     }
 
     @Override
@@ -27,6 +30,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this,"Volley学习",Toast.LENGTH_LONG).show();
                 Intent intent=new Intent(MainActivity.this, VolleyActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.videoToBase64_bt:
+                Toast.makeText(MainActivity.this,"视屏转Base64",Toast.LENGTH_LONG).show();
+                Intent intent_video =new Intent(MainActivity.this, VideoToBase64Demo.class);
+                startActivity(intent_video);
                 break;
             default:break;
         }

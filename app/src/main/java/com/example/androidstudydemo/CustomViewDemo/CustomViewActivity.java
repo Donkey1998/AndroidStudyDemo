23617926a768ad8997a7ca8055e4c4ace30530a4
Tony.li <tony.li@gaiaworks.cn>
2020-06-12 15:57:38 +0800

    自定义View onMeasure onDraw方法的简单使用

---
+++ app/src/main/java/com/example/androidstudydemo/CustomViewDemo/CustomViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidstudydemo.CustomViewDemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidstudydemo.R;
+
+public class CustomViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view);
+    }
+}
+

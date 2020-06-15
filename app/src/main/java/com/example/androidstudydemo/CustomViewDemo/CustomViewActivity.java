0e6bb67e7eff4f3f467f15f40ba252dab29d51e4
Tony.li <tony.li@gaiaworks.cn>
2020-06-15 16:49:35 +0800

    自定义组合控件

--- app/src/main/java/com/example/androidstudydemo/CustomViewDemo/CustomViewActivity.java
+++ app/src/main/java/com/example/androidstudydemo/CustomViewDemo/CustomViewActivity.java
@@ -2,6 +2,8 @@ package com.example.androidstudydemo.CustomViewDemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.androidstudydemo.R;
 
@@ -11,6 +13,22 @@ public class CustomViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view);
+        CustomTitleView  mTitleBar= (CustomTitleView) this.findViewById(R.id.title);
+//      mTitleBar.setTitle("自定义组合控件");
+
+        mTitleBar.setLeftListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CustomViewActivity.this, "点击左键", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mTitleBar.setRightListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CustomViewActivity.this, "点击右键", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
